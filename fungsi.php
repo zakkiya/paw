c2f4d98e1d6fff4c762712f593ea2a4d71cf0556
zakkiya <fnszakkiya@gmail.com>
2022-03-10 21:20:51 +0700

    menambahkan file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nomor3</title>
+</head>
+
+<body>
+    <?php
+    function perkalian($angka1,$angka2,$angka3){
+        echo $angka1*$angka2*$angka3;
+    }
+    echo perkalian(10,20,30) . "</br>";
+    echo perkalian(20,20,40) . "</br>";
+    echo perkalian(10,2,30) ;
+    ?>
+</body>
+
+</html>
