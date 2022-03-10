f68b7784056fd7858e10853eee2f0bf1d4f486b0
zakkiya <fnszakkiya@gmail.com>
2022-03-10 21:18:33 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nomor2</title>
+</head>
+
+<body>
+    <?php
+    //penjumlahan
+    $m= 10;
+    $n=20;
+    $o= 30;
+    $hasil = $m+$n+$o;
+    echo "hasil penjumlahan adalah=";
+    echo $hasil;
+    ?>
+    <br>
+    <?php
+    $p = 50;
+    $q= 10;
+    $r= 25;
+    $hasil = $p-$q-$r;
+    echo "hasil pengurangan adalah=";
+    echo $hasil;
+    ?>
+    <br>
+    <?php
+    $a = 50;
+    $b= 40;
+    $c= 5;
+    $hasil = $p*$q*$r;
+    echo "hasil perkalian adalah=";
+    echo $hasil;
+    ?>
+
+</body>
+
+</html>
