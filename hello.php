f210f4234eb96e65e0e88b89adf598aa50edaab9
zakkiya <fnszakkiya@gmail.com>
2022-03-10 20:51:46 +0700

    menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nomor1</title>
+</head>
+
+<body>
+    <?php
+    echo "NAMA : Zakkiya Fitri Nur Sa'adah </br>";
+    echo "NIM : 200411100097";
+    //menampilkan nama dan nim
+    ?>
+</body>
+
+</html>
